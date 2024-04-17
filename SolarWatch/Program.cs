942eed85ef970b3e64076f4f4cd7597cc7b4b67b
Papp Dániel <pappdani2@gmail.com>
2024-04-17 18:52:53 +0200

    Add Db Context for SolarWatchContext.cs in Program.cs

--- SolarWatch/Program.cs
+++ SolarWatch/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
 using SolarWatch.Service;
+using SolarWatch.Service.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SolarWatchContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,9 +15,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers(
     options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+builder.Services.AddScoped<ICityRepository, CityRepository>();
+builder.Services.AddScoped<ISunsetSunriseRepository, SunsetSunriseRepository>();
 builder.Services.AddSingleton<ICoordinateProvider, GeoCodingApi>();
 builder.Services.AddSingleton<ISunDataProvider, SunriseSunsetApi>();
 builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+builder.Services.AddDbContext<SolarWatchContext>();
 
 var app = builder.Build();
 
@@ -30,5 +38,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseDeveloperExceptionPage();
+
 
 app.Run();
