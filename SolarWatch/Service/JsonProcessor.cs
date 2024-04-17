cc7cf3a5acc29907d24003702feee5f07453c847
Papp Dániel <pappdani2@gmail.com>
2024-04-17 18:52:02 +0200

    more detailed JSON request

--- SolarWatch/Service/JsonProcessor.cs
+++ SolarWatch/Service/JsonProcessor.cs
@@ -1,33 +1,38 @@
 using System.Text.Json;
+using SolarWatch.Model;
 
 namespace SolarWatch.Service;
 
 public class JsonProcessor : IJsonProcessor
 {
-    public List<double> ProcessLatLon(string data)
+    public City ProcessCity(string data)
     {
-        var coords = new List<double>();
         JsonDocument json = JsonDocument.Parse(data);
         JsonElement main = json.RootElement[0];
 
         var lat = main.GetProperty("lat").GetDouble();
         var lon = main.GetProperty("lon").GetDouble();
-        coords.Add(lat);
-        coords.Add(lon);
-        return coords;
+        var name = main.GetProperty("name").GetString();
+        var stateProperty = main.TryGetProperty("state", out var stateElement);
+        var state = stateProperty ? stateElement.GetString() : null;
+        var country = main.GetProperty("country").GetString();
+        var id = Guid.NewGuid();
+        return new City { Id = id, Country = country, Name = name, Latitude = lat, Longitude = lon, State = state};
     }
 
-    public SolarWatchResponse ProcessSunriseSunset(string data)
+    public SunsetSunrise ProcessSunriseSunset(string data, City city)
     {
         JsonDocument json = JsonDocument.Parse(data);
         JsonElement results = json.RootElement.GetProperty("results");
 
-        SolarWatchResponse solarWatchResponse = new SolarWatchResponse
+        SunsetSunrise sunsetSunrise = new SunsetSunrise
         {
+            Id = Guid.NewGuid(),
             Sunrise = results.GetProperty("sunrise").GetString(),
-            Sunset = results.GetProperty("sunset").GetString()
+            Sunset = results.GetProperty("sunset").GetString(),
+            City = city
         };
-        return solarWatchResponse;
+        return sunsetSunrise;
     }
     
 }
