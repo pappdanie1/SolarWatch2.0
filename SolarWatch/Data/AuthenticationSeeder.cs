ea0d325e46b469b32281ff7f1a90fb4bf7957b4f
Papp Dániel <pappdani2@gmail.com>
2024-05-13 08:36:20 +0200

    auth seeder

---
+++ SolarWatch/Data/AuthenticationSeeder.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SolarWatch.Data;
+
+public class AuthenticationSeeder
+{
+    private RoleManager<IdentityRole> _roleManager;
+    private UserManager<IdentityUser> _userManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthenticationSeeder(RoleManager<IdentityRole> roleManager, IConfiguration configuration, UserManager<IdentityUser> userManager)
+    {
+        _roleManager = roleManager;
+        _configuration = configuration;
+        _userManager = userManager;
+    }
+
+    public void AddRoles()
+    {
+        var tAdmin = CreateAdminRole(_roleManager);
+        tAdmin.Wait();
+
+        var tUser = CreateUserRole(_roleManager);
+        tUser.Wait();
+    }
+    
+    public void AddAdmin()
+    {
+        var tAdmin = CreateAdminIfNotExists();
+        tAdmin.Wait();
+    }
+
+    private async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+    {
+        var role = Environment.GetEnvironmentVariable("ROLES__1") ?? _configuration["Roles:1"];
+        await roleManager.CreateAsync(new IdentityRole(role));
+    }
+    
+    async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
+    {
+        var role = Environment.GetEnvironmentVariable("ROLES__2") ?? _configuration["Roles:2"];
+        await roleManager.CreateAsync(new IdentityRole(role));
+    }
+    
+    private async Task CreateAdminIfNotExists()
+    {
+        var adminInDb = await _userManager.FindByEmailAsync("admin@admin.com");
+        if (adminInDb == null)
+        {
+            var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.com" };
+            var adminCreated = await _userManager.CreateAsync(admin, "admin123");
+
+            if (adminCreated.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+}
