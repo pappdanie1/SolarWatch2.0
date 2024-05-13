285f16cc87ba29c25fee0b3b9086a967700e9961
Papp Dániel <pappdani2@gmail.com>
2024-05-13 08:35:18 +0200

    id from init to set

--- SolarWatch/Model/City.cs
+++ SolarWatch/Model/City.cs
@@ -2,7 +2,7 @@ namespace SolarWatch.Model;
 
 public class City
 {
-    public Guid Id { get; init; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public double Longitude { get; set; }
     public double Latitude { get; set; }
